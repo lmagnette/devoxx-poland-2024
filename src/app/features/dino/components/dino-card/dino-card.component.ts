03f0819b084b81f1f69aba8dbb9a3e0d2145efd8
Loic Magnette <loic.magnette@gmail.com>
2024-06-17 15:00:12 +0200

    #4 [SIGNAL] basics

--- src/app/features/dino/components/dino-card/dino-card.component.ts
+++ src/app/features/dino/components/dino-card/dino-card.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input} from '@angular/core';
+import {Component, effect, inject, input, Input, signal} from '@angular/core';
 import {Dino} from "../../models/dino";
 import {NgOptimizedImage, NgSwitch, NgSwitchCase} from "@angular/common";
 import {MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
 import {RouterLink} from "@angular/router";
 import {MatBadge} from "@angular/material/badge";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-dino-card',
@@ -26,8 +27,18 @@ export class DinoCardComponent {
   @Input({required:true})
   dino!:Dino;
 
+  likeCount = signal<number>(0);
+
+  private snack = inject(MatSnackBar)
+
+  constructor() {
+    effect(() =>{
+      this.snack.open(`${this.dino.name} has been liked ${this.likeCount()}`);
+    })
+  }
+
 
   like() {
-
+    this.likeCount.update( value => value+1);
   }
 }
